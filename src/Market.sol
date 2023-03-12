d18e16c457ce056d7a0dc8b6e8236cf4d38e2111
caixiangfan <caixiang@ualberta.ca>
2023-03-11 23:30:27 -0700

    init market contract

---
+++ src/Market.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract Market {
+  uint256 private maxPrice;
+  struct Offer {
+    uint256 amount;
+    uint256 price;
+    string location;
+  }
+
+  struct Bid {
+    uint256 amount;
+    uint256 price;
+  }
+
+  modifier isValidOffer(uint256 _price) {
+    require(_price <= maxPrice);
+    _;
+  }
+
+  mapping(address => Offer) public offers;
+  mapping(address => Bid) public bids;
+
+  constructor(uint256 _maxPrice) {
+    maxPrice = _maxPrice;
+  }
+
+  function charge() public {
+    // Token consumer => smartContract => provider
+  }
+}
